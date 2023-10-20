ab0815fd66a21f42102cce6a44372938e02c5eea
AZOLTANS <anna.zoltanska@capgemini.com>
2023-10-20 14:32:01 +0200

    pdf download

--- client/src/app/onepager/onepager.component.ts
+++ client/src/app/onepager/onepager.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'onepager',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./onepager.component.scss']
 })
 export class OnepagerComponent {
+
+
+  downloadOnePager(){
+    var data = document.getElementById('onepager')!;
+    html2canvas(data).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jspdf('landscape', 'mm', 'a4');
+      pdf.addImage(imgData, 'PNG', 0, 0, 297, 210); // Adjust dimensions as needed
+      pdf.save('horizontal.pdf');
+    });
+  }
 
 }
